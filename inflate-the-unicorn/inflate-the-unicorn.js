bf76c7d1f83ca9f872a8061e1c6331ab9c325c74
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-10 10:26:56 +1200

    Makes images clickable

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,20 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+//MAKE IMAGES ARRAY
+let images = document.getElementsByTagName('img');
+
+
+//FUNCTIONS
+
+//handle click on images
+
+function imageClicked(e) {
+  let image = e.target;
+  console.log('clicked!')
+}
+
+for (let i = 0; i < images.length; i++) {
+  images[i].onclick = imageClicked;
+}
