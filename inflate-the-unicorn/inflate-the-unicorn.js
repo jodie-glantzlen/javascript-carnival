aace3eeacb23166a23f6520533b141d1d127828d
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-10 11:10:12 +1200

    Test one image change

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -15,8 +15,12 @@ let images = document.getElementsByTagName('img');
 function imageClicked(e) {
   let image = e.target;
   console.log('clicked!')
+  //test: when clicking on an image, change the image source:
+  image.src = 'images/unicorn-1.png';
 }
 
 for (let image of images) {
   image.onclick = imageClicked;
 }
+
+
