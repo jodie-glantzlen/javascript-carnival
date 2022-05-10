d6a03890dcb410ab60944732aed21414a724a605
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-10 17:11:09 +1200

    Update pseudocode to explain my train of thought

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -11,10 +11,13 @@ let images = document.getElementsByTagName('img');
 
 //create unicorns object
 //each key matches the img's id, so we can use it to manipulate it dynamically in template literals
-//each key's value is a state from 0 to 3, which corresponds to the img we want to give it
+//each count is a state from 0 to 3, which corresponds to:
+  //the number of times it's been clicked
+  //AND the img we want to give it, again using template literals
 //when an img is clicked:
-//update the relevant state so if you click unicorn1, unicorns.unicorn-1 += 1 (which corresponds to unicorn-1.png)
-//AND update img's src using template literals?
+  //update the relevant state so if you click unicorn1, unicorns.unicorn-1.count += 1 
+  //(which corresponds to unicorn-1.png AND img's id)
+  //AND update img's src using template literals?
 
 const unicorns = {
   "unicorn-1": {
@@ -50,4 +53,7 @@ for (let image of images) {
   image.onclick = imageClicked;
 }
 
+//It works! I don't know if using the same name for three different things (img id, the actual img's name and the unicorn reference) is good practice
+//but I wanted to try a dynamic, DRY and short solution
+//because my initial thought was 'oh we can use if statements" but... I've already practiced that enough I think
 
