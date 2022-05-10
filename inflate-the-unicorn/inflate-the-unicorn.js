d6ecb6b1b02fcd329d431b355b04307b4db4794e
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-10 16:33:51 +1200

    Add main feature

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -4,19 +4,31 @@
 
 console.log("Inflate The Unicorn!")
 
-//MAKE IMAGES ARRAY
+//Get all the images
 let images = document.getElementsByTagName('img');
 
+//main feature
 
-//FUNCTIONS
+//create unicorns object
+//each key matches the img's id, so we can use it to manipulate it dynamically in template literals
+//each key's value is a state from 0 to 3, which corresponds to the img we want to give it
+//when an img is clicked:
+//update the relevant state so if you click unicorn1, unicorns.unicorn-1 += 1 (which corresponds to unicorn-1.png)
+//AND update img's src using template literals?
+
+const unicorns = {
+  "unicorn-1": 0,
+  "unicorn-2": 0,
+  "unicorn-3": 0
+}
 
 //handle click on images
 
 function imageClicked(e) {
   let image = e.target;
-  console.log('clicked!')
-  //test: when clicking on an image, change the image source:
-  image.src = 'images/unicorn-1.png';
+  unicorns[image.id] += 1;
+  //console.log(`${unicorns[image.id]}`)
+  document.getElementById(`${image.id}`).src = `images/unicorn-${unicorns[image.id]}.png`;
 }
 
 for (let image of images) {
