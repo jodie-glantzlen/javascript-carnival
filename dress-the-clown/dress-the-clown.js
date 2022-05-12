be6e213ff8c5e0ad648f40b9d6bd8b41f25ea2ad
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-12 17:05:48 +1200

    Update head image cycling to remove small bugs

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,33 +1,37 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
 
 // gobal variables to reuse
-let headIndex = 0;
-let bodyIndex = 0;
-let shoesIndex = 0;
+let headIndex = 0
+let bodyIndex = 0
+let shoesIndex = 0
 
 // get body parts
 let head = document.getElementById('head')
 let body = document.getElementById('body')
 let shoes = document.getElementById('shoes')
 
-// change clown head 
+// change clown head
 function changeClownHead() {
   head.src = `images/head${headIndex}.png`
 }
 
-//change heads on arrow right key
+//change heads on arrow right and left key
 document.addEventListener('keydown', (e) => {
   if (e.key == 'ArrowRight') {
-    changeClownHead();
-    headIndex += 1
+    headIndex++
     if (headIndex > 5) {
-      headIndex = 0;
-      console.log('headindex: ', headIndex)
+      headIndex = 0
+    }
+    changeClownHead()
+  } else if (e.key == 'ArrowLeft') {
+    headIndex--
+    if (headIndex < 0) {
+      headIndex = 5
+    }
+    changeClownHead()
   }
-}
-});
-
+})
