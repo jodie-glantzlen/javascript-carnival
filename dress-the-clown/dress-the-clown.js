6506aa1494d96ea4f021a49a9aebb5ec2a1f5222
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-12 14:04:22 +1200

    Change clown head with first img

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,13 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+//1. change head of the clown with image 1
+
+let head = document.getElementById('head')
+
+function changeClownHead() {
+  head.src = 'images/head1.png'
+}
+
+changeClownHead()
