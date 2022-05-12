60365b9e99c2c981eeac784ae74f95bfecb842e6
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-05-12 14:43:05 +1200

    Add template literals with images index

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,12 +4,24 @@
 
 console.log("Dress The Clown!")
 
-//1. change head of the clown with image 1
+// gobal variables to reuse
+let headIndex = 0;
+let bodyIndex = 0;
+let shoesIndex = 0;
 
+// get body parts
 let head = document.getElementById('head')
+let body = document.getElementById('body')
+let shoes = document.getElementById('shoes')
+
+// change head of the clown with image 1
+
 
 function changeClownHead() {
-  head.src = 'images/head1.png'
+  head.src = `images/head${headIndex}.png`
+  headIndex += 1
+  //console.log('head index: ', headIndex)
 }
+
 
 changeClownHead()
